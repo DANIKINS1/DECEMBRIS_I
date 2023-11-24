7d7b353e1f6be1ad6bb8e4bef0b7defa96ca4dc3
DANIKINS1 <126759232+DANIKINS1@users.noreply.github.com>
2023-11-24 14:18:22 +0200

    s

---
+++ 1.uzdevums.py
@@ -0,0 +1,13 @@
+
+
+
+sakums=int(input("Ievadiet skaitli:"))
+
+
+sakuma_skaitlis=1
+
+
+
+for i in range(1, sakums+1):
+    sakuma_skaitlis +=1
+    print(i)
---
+++ 2.uzdevums.py
@@ -0,0 +1,10 @@
+skaitlis=1
+
+
+while skaitlis**2 <= 1000:
+    skaitlis +=1
+
+
+
+
+print(f"Skaitlis kura kvardrats ir lielaks par 1000 ir: {skaitlis}")
